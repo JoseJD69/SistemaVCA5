faa2ebcaea8175f594a0acdfdf5ea81abbf7812a
Jose Ibadango <joseijd69@gmail.com>
2018-05-01 21:16:44 -0500

    -chart bar with dinamic groups

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ var path = require("path");
 var bodyParser = require('body-parser');
 var mongo = require("mongoose");
 
-var db = mongo.connect("mongodb://localhost:27017/Tienda", function(err, response){
-    if(err){ console.log( err); }
-    else{ console.log('Connected to ' + db, ' + ', response); }
+var db = mongo.connect("mongodb://localhost:27017/donorschoose", function (err, response) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log('Connected to ' + db, ' + ', response);
+    }
 });
 
 
 var app = express()
 app.use(bodyParser());
-app.use(bodyParser.json({limit:'5mb'}));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json({limit: '500mb'}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.use(function (req, res, next) {
@@ -26,62 +30,65 @@ app.use(function (req, res, next) {
 var Schema = mongo.Schema;
 
 var ProductosSchema = new Schema({
-    nombre: { type: String   },
-    precio: { type: Number   },
-},{ versionKey: false });
+	_projectid: {type: Number},
+    school_latitude: {type: Number},
+    school_longitude: {type: Number},
+    school_city: {type: String},
+    school_state: {type: String},
+    school_metro: {type: String},
+    total_donations: {type: Number}
+}, {versionKey: false});
 
 
-var model = mongo.model('productos', ProductosSchema, 'productos');
+var model = mongo.model('projects', ProductosSchema, 'projects');
 
-app.post("/api/SaveProducto",function(req,res){
+app.post("/api/SaveProducto", function (req, res) {
     var mod = new model(req.body);
-    if(req.body.mode =="Save")
-    {
-        mod.save(function(err,data){
-            if(err){
+    if (req.body.mode == "Save") {
+        mod.save(function (err, data) {
+            if (err) {
                 res.send(err);
             }
-            else{
-                res.send({data:"Record has been Inserted..!!"});
+            else {
+                res.send({data: "Record has been Inserted..!!"});
             }
         });
     }
-    else
-    {
-        model.findByIdAndUpdate(req.body.id, { nombre: req.body.nombre, precio: req.body.precio},
-            function(err,data) {
+    else {
+        model.findByIdAndUpdate(req.body.id, {nombre: req.body.nombre, precio: req.body.precio},
+            function (err, data) {
                 if (err) {
                     res.send(err);
                 }
-                else{
-                    res.send({data:"Record has been Updated..!!"});
+                else {
+                    res.send({data: "Record has been Updated..!!"});
                 }
             });
     }
 })
 
-app.post("/api/deleteProducto",function(req,res){
-    model.remove({ _id: req.body.id }, function(err) {
-        if(err){
+app.post("/api/deleteProducto", function (req, res) {
+    model.remove({_id: req.body.id}, function (err) {
+        if (err) {
             res.send(err);
         }
-        else{
-            res.send({data:"Record has been Deleted..!!"});
+        else {
+            res.send({data: "Record has been Deleted..!!"});
         }
     });
 })
 
 
-
-app.get("/api/getProducto",function(req,res){
-    model.find({},function(err,data){
-        if(err){
+app.get("/api/getProducto", function (req, res) {
+		
+    model.find({}, function (err, data) {
+        if (err) {
             res.send(err);
         }
-        else{
+        else {
             res.send(data);
         }
-    });
+    }).limit(400);
 })
 
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import {CommonService} from './common.service';
 import * as d3 from 'd3';
 import * as c3 from 'c3';
 import {JsonFormatter} from 'tslint/lib/formatters';
+import {el} from '@angular/platform-browser/testing/src/browser_util';
+import {variable} from '@angular/compiler/src/output/output_ast';
 
 @Component({
     selector: 'app-root',
@@ -15,6 +17,8 @@ export class AppComponent implements OnInit {
     constructor(private newService: CommonService) {
     }
 
+    chart;
+    group = [];
     data = [{letter: 'A', frequency: .08167},
         {letter: 'B', frequency: .01492},
         {letter: 'C', frequency: .02782},
@@ -22,31 +26,38 @@ export class AppComponent implements OnInit {
         {letter: 'E', frequency: .12702}];
     Repdata;
     valbutton = 'Save';
+
     ids = [];
 
 
     ngOnInit() {
         this.newService.GetProducto().subscribe(data => {
             this.Repdata = data;
-            this.createbarC3(data);
+            console.log(data);
+            //   this.createbarC3(data);
         });
     }
 
-    createbarC3(data) {
-
-        const chart = c3.generate({
+    createbarC3(data, variableC) {
+        this.chart = c3.generate({
             bindto: '#chart',
             data: {
+                groups: [
+                    ['school_latitude']
+                ],
                 json: data,
                 keys: {
-                    x: 'nombre', // it's possible to specify 'x' when category axis
-                    value: ['precio'],
+                    //   x: '_projectid', // it's possible to specify 'x' when category axis
+                    value: variableC
                 },
-                type: 'bar'
+
+            },
+            zoom: {
+                enabled: true
             },
             axis: {
                 x: {
-                    type: 'category'
+                    // type: 'category'
                 }
             },
             bar: {
@@ -68,6 +79,30 @@ export class AppComponent implements OnInit {
                 , error => this.errorMessage = error);
 
     };
+
+    onChangeVal(ev) {
+
+        if (ev.currentTarget.checked) {
+            const val = ev.target.defaultValue;
+            console.log('checkado');
+            switch (val) {
+                case 'school_latitude':
+                    this.group.push(val);
+                    console.log(this.group);
+                    this.createbarC3(this.Repdata, this.group);
+                    break;
+                case 'total_price_excluding_optional_support':
+                    this.group.push(val);
+                    this.createbarC3(this.Repdata,this.group);
+                    break;
+
+
+            }
+        } else {
+            console.log('descheckado');
+        }
+    }
+
     edit = function (product) {
         this.id = product._id;
         this.nombre = product.nombre;
